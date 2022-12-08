867744085b21803659f7ae002f48b9dc87de61b5
Dluter <tom230023@gmail.com>
2022-12-08 22:34:09 +0800

    test

--- src/pages/Shop/ProductData.ts
+++ src/pages/Shop/ProductData.ts
@@ -1,16 +1,17 @@
-import IMG_8811 from '@assets/shop/IMG_8811.jpg';
+import IMG_8859 from '@assets/shop/IMG_8859.jpg';
+import IMG_8904 from '@assets/shop/IMG_8904.jpg';
 
 const ProductData = [
   {
     title: '青皮',
-    imgPaths: [IMG_8811],
+    imgPaths: [IMG_8904],
     labels: ['熟了會泛黃不變色', '圓胖型', '紋路不清楚'],
     price: { gram: 10, value: 700 },
     remark: '約10-12顆，含宅配運費',
   },
   {
     title: '黑美人',
-    imgPaths: [IMG_8811],
+    imgPaths: [IMG_8859],
     labels: ['紋路清晰', '長橢圓形', '熟了會變紫黑色'],
     price: { gram: 10, value: 700 },
     remark: '約10-12顆，含宅配運費',
